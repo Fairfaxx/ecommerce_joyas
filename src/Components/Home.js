b54e0a8bb0562c4b8a2341b098a40bd012b44b69
Fede Lemaire <fede.lemaire@gmail.com>
2021-02-25 15:54:01 -0300

    app terminada

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,10 +4,13 @@ import ItemListContainer from '../Containers/ItemListContainer';
 const Home = () => {
 
     return (
-        <div>
+        <div style={{ paddingTop: '50px' }}>
             <h1>Joyas Antiguas de Colección</h1>
             <h3>Desde 1973</h3>
-            <ItemListContainer />
+            <p style={{ paddingTop: '50px' }}>Estamos en el <strong>mismo lugar</strong> desde siempre. <br /> Nuestro <strong>puntaje</strong> nos valida como la <strong>MEJOR</strong> casa de ventas de Joyas</p>
+            <div style={{ marginTop: '-130px' }}>
+                <ItemListContainer />
+            </div>
         </div>
     );
 }
